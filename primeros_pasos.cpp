98d251f23020ceb699adb7ecdec7ab7199568efc
Paolo Belforte <pbelforte2003@gmail.com>
2022-07-22 10:47:34 -0300

    commit

---
+++ primeros_pasos.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int edad = 10;
+    float flotante = 10.5;
+    double doble = 10.43534;
+    char caracter = 'a';
+
+    cout << "Hola mundo! \n";
+
+    cout << "Cual es tu edad?";
+
+    cin >> edad;
+
+    cout << "Edad es : " << edad;
+
+    return 0;
+}
